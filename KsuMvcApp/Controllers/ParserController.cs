4c80ab851fb0bbdb945e34af9cf10efb25d74cf1
James Evans <sir.kravtsov2016@gmail.com>
2019-07-31 21:51:22 +0300

    Update 1

---
+++ KsuMvcApp/Controllers/ParserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KsuMvcApp.Controllers
+{
+    public class ParserController : Controller
+    {
+        public ActionResult FindExpression()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult FindExpression(String expression)
+		{
+			// Вызов метода из парсера
+			return View();
+		}
+	}
+}
